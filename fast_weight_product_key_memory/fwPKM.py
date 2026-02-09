4bf9effa5dd798fac99e0e0edeacf0095ad1a1f6
lucidrains <lucidrains@gmail.com>
2026-02-09 07:01:16 -0800

    flesh out

--- fast_weight_product_key_memory/fwPKM.py
+++ fast_weight_product_key_memory/fwPKM.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import torch
+from torch import tensor, Tensor
 from torch.nn import Module, ModuleList, RMSNorm
 
 import einx
@@ -14,6 +15,14 @@ def exists(v):
 def default(v, d):
     return v if exists(v) else d
 
+# tensor helpers
+
+def log(t, eps = 1e-20):
+    return t.clamp_min(eps).log()
+
+def entropy(prob):
+    return -(prob * log(prob)).sum(dim = -1)
+
 # classes
 
 class fwPKM(Module):
@@ -25,9 +34,12 @@ class fwPKM(Module):
         dim_v = 512,
         learning_rate = 1.,
         topk = 8,
-        lookahead_values = True
+        lookahead_values = True,
+        addressing_loss_weight = 10.
     ):
         super().__init__()
+
+        self.register_buffer('zero', tensor(0.), persistent = False)
 
     def forward(
         self,
