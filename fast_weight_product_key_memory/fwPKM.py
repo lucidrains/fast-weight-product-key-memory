e62ced4ac0e7eb0ef65408bed0c6bfbf6a9fa471
lucidrains <lucidrains@gmail.com>
2026-02-09 07:29:15 -0800

    prepare all parameters

--- fast_weight_product_key_memory/fwPKM.py
+++ fast_weight_product_key_memory/fwPKM.py
@@ -1,11 +1,14 @@
 from __future__ import annotations
+from math import sqrt
 
 import torch
+from torch import nn
 from torch import tensor, Tensor
-from torch.nn import Module, ModuleList, RMSNorm
+from torch.nn import Module, Sequential, RMSNorm
 
 import einx
 from einops import rearrange
+from einops.layers.torch import Rearrange
 
 # functions
 
@@ -14,6 +17,9 @@ def exists(v):
 
 def default(v, d):
     return v if exists(v) else d
+
+def LinearNoBias(dim, dim_out):
+    return nn.Linear(dim, dim_out, bias = False)
 
 # tensor helpers
 
@@ -30,14 +36,48 @@ class fwPKM(Module):
         self,
         dim,
         *,
-        dim_qk = 512,
-        dim_v = 512,
+        num_memories = 512 * 512,
+        dim_queries_keys = 512,
+        dim_values = 512,
         learning_rate = 1.,
         topk = 8,
         lookahead_values = True,
         addressing_loss_weight = 10.
     ):
         super().__init__()
+        assert sqrt(num_memories).is_integer(), 'num memories must have an integer square root'
+
+        self.memories = nn.Parameter(torch.randn(num_memories, dim_values) * 1e-2)
+
+        num_keys = int(sqrt(num_memories))
+        self.keys = nn.Parameter(torch.randn(2, num_keys, dim_queries_keys))
+
+        # projections
+
+        self.to_queries = Sequential(
+            RMSNorm(dim),
+            LinearNoBias(dim, dim_queries_keys * 2),
+            Rearrange('... (two d) -> two ... d', two = 2)
+        )
+
+        self.to_gates = Sequential(
+            RMSNorm(dim),
+            LinearNoBias(dim, 1)
+        )
+
+        self.to_values = Sequential(
+            RMSNorm(dim),
+            LinearNoBias(dim, dim_values)
+        )
+
+        self.to_out = Sequential(
+            RMSNorm(dim_values),
+            LinearNoBias(dim_values, dim)
+        )
+
+        # loss related
+
+        self.addressing_loss_weight = addressing_loss_weight
 
         self.register_buffer('zero', tensor(0.), persistent = False)
 
@@ -45,4 +85,5 @@ class fwPKM(Module):
         self,
         tokens
     ):
+
         return tokens
--- tests/test_memory.py
+++ tests/test_memory.py
@@ -1,7 +1,14 @@
 import pytest
 param = pytest.mark.parametrize
 
+import torch
+
 def test_memory():
     from fast_weight_product_key_memory.fwPKM import fwPKM
 
-    assert True
+    pkm = fwPKM(512)
+
+    tokens = torch.randn(2, 256, 512)
+    out = pkm(tokens)
+
+    assert tokens.shape == out.shape
